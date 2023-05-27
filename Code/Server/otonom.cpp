7ec037a22fc32721c7676cdd5995fd50e3956fa2
CSE396 <atacanbasaran74@gmail.com>
2023-05-27 15:43:49 +0300

    mesaj

--- Code/Server/otonom.cpp
+++ Code/Server/otonom.cpp
@@ -16,73 +16,72 @@ void callforward();
 int main(int argc, char* argv[])
 {
 	
-		//int yön 1
-		
-		pthread_t thread1 , thread2;
-		
-		pthread_create(&thread1, NULL , func1 , NULL);
-		pthread_create(&thread2, NULL , func2 , NULL);
-		
-		pthread_join(thread1 , NULL);
-		pthread_join(thread2 , NULL);
-		
-		//sağa/sola dön
-		//10 adım
-		//sağa döndüyse dola,sola döndüyse sağa
-		//eğer engel yoksa devam
-		//engel varsa ilk döndüğü tarafa 
-		//10 adım içinde engele rastlarsa
-		//180 derece ters tarafa
-		//aynı işlemleri diğer yöne yapcak
-		
-		return 0;
+	//int yön 1
+
+	pthread_t thread1 , thread2;
+
+	pthread_create(&thread1, NULL , func1 , NULL);
+	pthread_create(&thread2, NULL , func2 , NULL);
+
+	pthread_join(thread1 , NULL);
+	pthread_join(thread2 , NULL);
+
+	//sağa/sola dön
+	//10 adım
+	//sağa döndüyse dola,sola döndüyse sağa
+	//eğer engel yoksa devam
+	//engel varsa ilk döndüğü tarafa 
+	//10 adım içinde engele rastlarsa
+	//180 derece ters tarafa
+	//aynı işlemleri diğer yöne yapcak
+	return 0;
 }
 
 
 void* func1(void* arg)
 {		
-		int distance = 10;
-		//char temp = "\n";
-		//wait(NULL);
-		int pid = fork();
-		if (pid == 0){
-			system("sudo python Control.py");
-			}
-		else{
-			wait(NULL);
-		while(distance  >= 15){
-		
-				ifstream inFile;
-				inFile.open("distance.txt");
-				inFile >> distance;
-				sleep(0.5);
-				inFile.close();
-			
-		}			
-			
+	int distance = 10;
+	//char temp = "\n";
+	//wait(NULL);
+	int pid = fork();
+	if (pid == 0){
+		system("sudo python Control.py");
 		}
+	else{
+		wait(NULL);
+	while(distance  >= 15){
 
-			
-		return NULL;
+			ifstream inFile;
+			inFile.open("distance.txt");
+			inFile >> distance;
+			sleep(0.5);
+			inFile.close();
+		
+	}			
+		
+	}
+
+		
+	return NULL;
 }
 
 void* func2(void* arg)
 {
-		const char* filename = "Ultrasonic.py";
-		FILE* fp;
-		
-		Py_Initialize();
-		
-		fp = fopen(filename, "r");
-		if(fp == NULL){
-				perror("failed to open file \n");
-				
-		}
-		
-		PyRun_SimpleFileEx(fp , "Ultrasonic.py" , 1);
-		
-		Py_Finalize();
-		return NULL;
+	const char* filename = "Ultrasonic.py";
+	FILE* fp;
+
+	Py_Initialize();
+
+	fp = fopen(filename, "r");
+	if(fp == NULL){
+			perror("failed to open file \n");
+			
+	}
+
+	PyRun_SimpleFileEx(fp , "Ultrasonic.py" , 1);
+
+	Py_Finalize();
+	return NULL;
 }
 
 void callforward(){
